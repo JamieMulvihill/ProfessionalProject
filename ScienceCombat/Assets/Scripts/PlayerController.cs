c1b27f693da93ff0c61509306564323c97e605c1
PhilipGooch <philip.daniel.gooch@gmail.com>
2020-01-28 16:30:07 +0000

    Re-adding EVERYTHING

---
+++ ScienceCombat/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    public float Speed
+    {
+        get
+        {
+            return this.speed;
+        }
+        set
+        {
+            this.speed = value;
+        }
+    }
+
+
+    public Rigidbody rigidBody;
+    [SerializeField] private string playerHOR, PlayerVer, PlayerInter, PlayerFire;
+    [SerializeField] private float rotationSpeed;
+
+    private void Start()
+    {
+        Debug.Log("Collide");
+        Debug.DrawLine(Vector3.zero, new Vector3(5, 0, 0), Color.green, 2.5f);
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 joystickDirection = new Vector3(Input.GetAxis(playerHOR), 0, Input.GetAxis(PlayerVer));
+
+        // If the stick is not at rest.
+        if (Mathf.Abs(Input.GetAxis(playerHOR)) > 0.01f || Mathf.Abs(Input.GetAxis(PlayerVer)) > 0.01f)
+        {
+            // Setting the rotation of the player to turn towards the direction of the joystick.
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(joystickDirection, transform.up), rotationSpeed);
+        }
+        // Setting the velocity of the player
+        rigidBody.velocity = transform.forward * new Vector3(Mathf.Abs(joystickDirection.x), 0, Mathf.Abs(joystickDirection.z)).magnitude * speed;
+    }
+
+
+}
---
+++ ScienceCombat/Assets/Scripts/VirusAttack.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirusAttack : MonoBehaviour{
+    public GameObject virus;
+    [SerializeField] private string playerFire;
+    [SerializeField] private float verticalVelocity;
+    [SerializeField] private float horizontalVelocity;
+    [SerializeField] private float fireRate = 1f;
+    private float lastShot = 0.0f;
+    GameObject inst;
+
+    public void Update() {
+
+        if (Mathf.Abs(Input.GetAxis(playerFire)) > 0.01f){
+            if (Time.time > fireRate + lastShot) {
+                Vector3 SpawnPoint = transform.position + (transform.forward * 2) + (transform.up * 2);
+
+                inst = Instantiate(virus, SpawnPoint, Quaternion.LookRotation(transform.forward, transform.up));
+                Rigidbody rigidBody = inst.GetComponent<Rigidbody>();
+                rigidBody.transform.position = SpawnPoint;
+                rigidBody.velocity = new Vector3(horizontalVelocity * transform.forward.x, verticalVelocity * (transform.forward.y + 1), horizontalVelocity * transform.forward.z);
+                // rigidBody.AddRelativeForce(new Vector3(horizontalVelocity * transform.forward.x, verticalVelocity * (transform.forward.y + 1), horizontalVelocity * transform.forward.z), ForceMode.Impulse);
+                lastShot = Time.time;
+            }
+        }
+    }
+}
