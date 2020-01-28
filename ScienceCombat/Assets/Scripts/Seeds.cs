70b80c39b0c4df31ddcbdde5c95deb12d782a936
Jamie <1703169@uad.ac.uk>
2020-01-28 17:58:47 +0000

    Added Jamie scene

---
+++ ScienceCombat/Assets/Scripts/Seeds.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seeds : MonoBehaviour
+{
+    [SerializeField] private GameObject vines;
+    // Start is called before the first frame update
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject inst;
+        inst = Instantiate(vines, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
--- ScienceCombat/Assets/Scripts/Vines.cs
+++ ScienceCombat/Assets/Scripts/Vines.cs
@@ -4,32 +4,26 @@ using UnityEngine;
 
 public class Vines : MonoBehaviour
 {
-    private float lifeTime = 0f;
     private bool spawned = false;
     private float previousSpeed;
     private GameObject capturedPlayer;
-    private void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.tag != "Biologist" && collision.gameObject.tag != "Ground")
-        {
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag != "Biologist" && collision.gameObject.tag != "Ground") {
             capturedPlayer = collision.gameObject;
             spawned = true;
             previousSpeed = capturedPlayer.GetComponent<PlayerController>().Speed;
             capturedPlayer.GetComponent<PlayerController>().Speed = 0;
-           
+            StartCoroutine(DEATH());
         }
-    }
-    private void Update(){
-        if (spawned)
-        {
-            lifeTime += Time.deltaTime;
-        }
-        Death();
     }
 
-    private void Death() {
-        if (lifeTime > 10f) {
-            capturedPlayer.GetComponent<PlayerController>().Speed = previousSpeed;
-            Destroy(gameObject);
-        }
+    IEnumerator DEATH() {
+        yield return new WaitForSeconds(5);
+        capturedPlayer.GetComponent<PlayerController>().Speed = previousSpeed;
+        Destroy(gameObject);
+
     }
+
+
 }
